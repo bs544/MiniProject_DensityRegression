6bef86f07ea8d620b17dacd29f3a5fdda9676d3d
bs544 <cdt1804@cdt18-laptop-04.lsc.phy.private.cam.ac.uk>
2019-03-11 11:14:11 +0000

    It looks like I need the __init__.py to make this an importable module, so that's what I'm doing. Also adding it to PYTHONPATH seemed like a good idea

---
+++ MiniProject_DensityRegression/__init__.py
@@ -0,0 +1,6 @@
+from MiniProject_DensityRegression.network import *
+from MiniProject_DensityRegression.fingerprints import *
+from MiniProject_DensityRegression.castep_density import *
+from MiniProject_DensityRegression.get_results import *
+__version__="0.0.1"
+
